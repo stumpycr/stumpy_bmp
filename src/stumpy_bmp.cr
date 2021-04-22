c96c59ccd3d14f2cc3b94942e37a9d0f01eb5912
Daniel Huffman <drhuffman12@gmail.com>
2021-04-22 03:23:34 -0400

    drhuffman12_update_stumpy_bmp split up the 'read' method

--- src/stumpy_bmp.cr
+++ src/stumpy_bmp.cr
@@ -65,16 +65,162 @@ module StumpyBMP
   end
 
   def self.read(filename)
+    # file = File.open(filename)
+    # file_bytes = [] of UInt8
+    # file_header = {} of Symbol => UInt32
+
+    # # file must be read as UInt8 bytes
+    # while (c = file.read_byte)
+    #   file_bytes << c
+    # end
+    file_bytes = read_file_bytes(filename)
+
+    # raise "Not a BMP file" if file_bytes[FILE_IDENT_HEADER_RANGE].map(&.chr).join != FILE_IDENT_HEADER
+    validate!(file_bytes)
+
+    # file_header[:file_size] = long_to_int file_bytes[FILE_SIZE_RANGE]
+    # file_header[:rs1] = bit16_to_int(file_bytes[FILE_RS1_RANGE]).to_u32
+    # file_header[:rs2] = bit16_to_int(file_bytes[FILE_RS2_RANGE]).to_u32
+    # file_header[:offset] = long_to_int file_bytes[FILE_OFFSET_RANGE]
+    # file_header[:header_size] = long_to_int file_bytes[IMAGE_HEADER_SIZE_RANGE]
+    # file_header[:width] = long_to_int file_bytes[IMAGE_WIDTH_RANGE]
+    # file_header[:height] = long_to_int file_bytes[IMAGE_HEIGHT_RANGE]
+    # file_header[:color_planes] = bit16_to_int(file_bytes[IMAGE_COLOR_PLANES_RANGE]).to_u32
+    # file_header[:bits] = bit16_to_int(file_bytes[IMAGE_BITS_RANGE]).to_u32
+    # file_header[:compression] = long_to_int file_bytes[IMAGE_COMPRESSION_RANGE]
+    # file_header[:size] = long_to_int file_bytes[IMAGE_SIZE_RANGE]
+    # file_header[:res_x] = long_to_int file_bytes[IMAGE_RESOLUTION_X_RANGE]
+    # file_header[:res_y] = long_to_int file_bytes[IMAGE_RESOLUTION_Y_RANGE]
+    # file_header[:color_numbers] = long_to_int file_bytes[IMAGE_COLOR_NUMBERS_RANGE]
+    # file_header[:important_colors] = long_to_int file_bytes[IMAGE_IMPORTANT_COLORS_RANGE]
+
+    file_header = extract_header_data(file_bytes)
+
+    # canvas = StumpyCore::Canvas.new(file_header[:width].to_i32, file_header[:height].to_i32)
+
+    # image_data_range = (file_header[:offset]...(file_header[:offset] + file_header[:width] * file_header[:height] * 3))
+    # # Get 3 bytes at a time
+
+    # (image_data_range.size // 3).times do |p|
+    #   x = (p % file_header[:width]).to_i32
+    #   # "invert" y because for some reason it stores the height backwards.
+    #   y = (file_header[:height] - 1 - (p / file_header[:width])).to_i32
+    #   # extra spaces to skip because rows are seperated by two 00 bytes
+    #   pos = (p / file_header[:width]) * 2
+
+    #   cr_from = to_u8_bounded(image_data_range.begin + (p * 3) + pos)
+
+    #   cr_to = to_u8_bounded(image_data_range.begin + (p * 3) + 2 + pos)
+
+    #   color_bytes = bit24_to_int file_bytes[cr_from..cr_to]
+
+    #   # r = (color_bytes >> 16).to_u16
+    #   # r = to_u8_bounded(color_bytes >> 16)
+    #   r = to_u8_bounded(color_bytes.bits(16..23))
+    #   # r = UInt16::MAX * (r / UInt8::MAX)
+
+    #   # g = (color_bytes >> 8).to_u16 & 0xFF
+    #   # g = to_u8_bounded((color_bytes >> 8) & 0xFF)
+    #   g = to_u8_bounded(color_bytes.bits(8..15))
+    #   # g = UInt16::MAX * (g / UInt8::MAX)
+
+    #   # b = (color_bytes).to_u16 & 0xFF
+    #   # b = to_u8_bounded(color_bytes & 0xFF)
+    #   b = to_u8_bounded(color_bytes.bits(0..7))
+    #   # b = UInt16::MAX * (b / UInt8::MAX)
+
+    #   canvas.safe_set(x, y, StumpyCore::RGBA.from_rgb8(r, g, b))
+    # end
+
+    canvas = populate_canvas(file_header, file_bytes)
+  end
+
+  def self.populate_canvas(file_header, file_bytes)
+    image_data_range = (file_header[:offset]...(file_header[:offset] + file_header[:width] * file_header[:height] * 3))
+    # Get 3 bytes at a time
+
+    canvas = StumpyCore::Canvas.new(file_header[:width].to_i32, file_header[:height].to_i32)
+
+    (image_data_range.size // 3).times do |p|
+      # x = (p % file_header[:width]).to_i32
+      # # "invert" y because for some reason it stores the height backwards.
+      # y = (file_header[:height] - 1 - (p / file_header[:width])).to_i32
+      # # extra spaces to skip because rows are seperated by two 00 bytes
+      # pos = (p / file_header[:width]) * 2
+
+      # cr_from = to_u8_bounded(image_data_range.begin + (p * 3) + pos)
+
+      # cr_to = to_u8_bounded(image_data_range.begin + (p * 3) + 2 + pos)
+
+      # color_bytes = bit24_to_int file_bytes[cr_from..cr_to]
+
+      # # r = (color_bytes >> 16).to_u16
+      # # r = to_u8_bounded(color_bytes >> 16)
+      # r = to_u8_bounded(color_bytes.bits(16..23))
+      # # r = UInt16::MAX * (r / UInt8::MAX)
+
+      # # g = (color_bytes >> 8).to_u16 & 0xFF
+      # # g = to_u8_bounded((color_bytes >> 8) & 0xFF)
+      # g = to_u8_bounded(color_bytes.bits(8..15))
+      # # g = UInt16::MAX * (g / UInt8::MAX)
+
+      # # b = (color_bytes).to_u16 & 0xFF
+      # # b = to_u8_bounded(color_bytes & 0xFF)
+      # b = to_u8_bounded(color_bytes.bits(0..7))
+      # # b = UInt16::MAX * (b / UInt8::MAX)
+
+      # canvas.safe_set(x, y, StumpyCore::RGBA.from_rgb8(r, g, b))
+      populate_pixel(file_header, file_bytes, image_data_range, canvas, p)
+    end
+
+    canvas
+  end
+
+  def self.populate_pixel(file_header, file_bytes, image_data_range, canvas, p)
+    x = (p % file_header[:width]).to_i32
+    # "invert" y because for some reason it stores the height backwards.
+    y = (file_header[:height] - 1 - (p / file_header[:width])).to_i32
+    # extra spaces to skip because rows are seperated by two 00 bytes
+    pos = (p / file_header[:width]) * 2
+
+    cr_from = to_u8_bounded(image_data_range.begin + (p * 3) + pos)
+
+    cr_to = to_u8_bounded(image_data_range.begin + (p * 3) + 2 + pos)
+
+    color_bytes = bit24_to_int file_bytes[cr_from..cr_to]
+
+    # r = (color_bytes >> 16).to_u16
+    # r = to_u8_bounded(color_bytes >> 16)
+    r = to_u8_bounded(color_bytes.bits(16..23))
+    # r = UInt16::MAX * (r / UInt8::MAX)
+
+    # g = (color_bytes >> 8).to_u16 & 0xFF
+    # g = to_u8_bounded((color_bytes >> 8) & 0xFF)
+    g = to_u8_bounded(color_bytes.bits(8..15))
+    # g = UInt16::MAX * (g / UInt8::MAX)
+
+    # b = (color_bytes).to_u16 & 0xFF
+    # b = to_u8_bounded(color_bytes & 0xFF)
+    b = to_u8_bounded(color_bytes.bits(0..7))
+    # b = UInt16::MAX * (b / UInt8::MAX)
+
+    canvas.safe_set(x, y, StumpyCore::RGBA.from_rgb8(r, g, b))
+  end
+
+  def self.read_file_bytes(filename)
     file = File.open(filename)
     file_bytes = [] of UInt8
-    file_header = {} of Symbol => UInt32
 
     # file must be read as UInt8 bytes
     while (c = file.read_byte)
       file_bytes << c
     end
 
-    raise "Not a BMP file" if file_bytes[FILE_IDENT_HEADER_RANGE].map(&.chr).join != FILE_IDENT_HEADER
+    file_bytes
+  end
+
+  def self.extract_header_data(file_bytes)
+    file_header = {} of Symbol => UInt32
 
     file_header[:file_size] = long_to_int file_bytes[FILE_SIZE_RANGE]
     file_header[:rs1] = bit16_to_int(file_bytes[FILE_RS1_RANGE]).to_u32
@@ -92,43 +238,11 @@ module StumpyBMP
     file_header[:color_numbers] = long_to_int file_bytes[IMAGE_COLOR_NUMBERS_RANGE]
     file_header[:important_colors] = long_to_int file_bytes[IMAGE_IMPORTANT_COLORS_RANGE]
 
-    colors = StumpyCore::Canvas.new(file_header[:width].to_i32, file_header[:height].to_i32)
+    file_header
+  end
 
-    image_data_range = (file_header[:offset]...(file_header[:offset] + file_header[:width] * file_header[:height] * 3))
-    # Get 3 bytes at a time
-
-    (image_data_range.size // 3).times do |p|
-      x = (p % file_header[:width]).to_i32
-      # "invert" y because for some reason it stores the height backwards.
-      y = (file_header[:height] - 1 - (p / file_header[:width])).to_i32
-      # extra spaces to skip because rows are seperated by two 00 bytes
-      pos = (p / file_header[:width]) * 2
-
-      cr_from = to_u8_bounded(image_data_range.begin + (p * 3) + pos)
-
-      cr_to = to_u8_bounded(image_data_range.begin + (p * 3) + 2 + pos)
-
-      color_bytes = bit24_to_int file_bytes[cr_from..cr_to]
-
-      # r = (color_bytes >> 16).to_u16
-      # r = to_u8_bounded(color_bytes >> 16)
-      r = to_u8_bounded(color_bytes.bits(16..23))
-      # r = UInt16::MAX * (r / UInt8::MAX)
-
-      # g = (color_bytes >> 8).to_u16 & 0xFF
-      # g = to_u8_bounded((color_bytes >> 8) & 0xFF)
-      g = to_u8_bounded(color_bytes.bits(8..15))
-      # g = UInt16::MAX * (g / UInt8::MAX)
-
-      # b = (color_bytes).to_u16 & 0xFF
-      # b = to_u8_bounded(color_bytes & 0xFF)
-      b = to_u8_bounded(color_bytes.bits(0..7))
-      # b = UInt16::MAX * (b / UInt8::MAX)
-
-      colors.safe_set(x, y, StumpyCore::RGBA.from_rgb8(r, g, b))
-    end
-
-    colors
+  def self.validate!(file_bytes)
+    raise "Not a BMP file" if file_bytes[FILE_IDENT_HEADER_RANGE].map(&.chr).join != FILE_IDENT_HEADER
   end
 
   def self.long_to_int(long_chars) : UInt32
